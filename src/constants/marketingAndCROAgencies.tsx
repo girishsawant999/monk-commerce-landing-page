3c23f76ecf2f26873978a292c8acf1a33f5c133d
Girish Sawant <girishsawant999.gs@gmail.com>
2025-06-01 21:08:53 +0530

    Add marketing and top brands logo URLs constants

---
+++ src/constants/marketingAndCROAgencies.tsx
@@ -0,0 +1,9 @@
+const MARKETING_AND_CRO_AGENCIES_LOGO_URLS = [
+  "https://framerusercontent.com/images/IpAQqL6hF5uihDE72hYnklU0v8o.png",
+  "https://framerusercontent.com/images/IXBoO9mAYxTI7uSQ6YmEBJC7mo.png",
+  "https://framerusercontent.com/images/IxNKls11BFSySqy42xYpLANeVI.png",
+  "https://framerusercontent.com/images/hcxTrtHZerxHSVG0jEvXZ1elkJg.png",
+  "https://framerusercontent.com/images/xtneojpKdh1q4EcblhsIKU5E.png",
+];
+
+export default MARKETING_AND_CRO_AGENCIES_LOGO_URLS;
---
+++ src/constants/topBrands.tsx
@@ -0,0 +1,11 @@
+const TOP_BRANDS_LOGOS_URLS = [
+  "https://framerusercontent.com/images/feZaXMdOY3FVnzvGIAQUbuoQ0fg.png",
+  "https://framerusercontent.com/images/fe08K3ktVNfVFy9zXzo8qVDQY.png",
+  "https://framerusercontent.com/images/86zv3c1HdDIBIba0rAWtRa6Zxro.png",
+  "https://framerusercontent.com/images/MFQGpWFCIg7sUNybPkU3SsI5YBc.png",
+  "https://framerusercontent.com/images/sIBmf1gZhPSwE5NRsSUQIRbRoE.png",
+  "https://framerusercontent.com/images/FkS3c5pK65SAUrRwnSq6ajiVRDw.png",
+  "https://framerusercontent.com/images/xVpZVkCoKuxcgxHwiT6XbZfZ3dg.png",
+];
+
+export default TOP_BRANDS_LOGOS_URLS;
